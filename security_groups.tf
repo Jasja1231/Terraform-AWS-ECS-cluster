1acb6f2a1b4161dc12bbf03e8fb78eb9b6a64ba1
jasja1231 <jasja1231@gmail.com>
2022-12-10 22:12:41 +0200

    security group tf

---
+++ security_groups.tf
@@ -0,0 +1,71 @@
+#public access 0.0.0.0 security group
+resource "aws_security_group" "terr_public_sec_group" {
+  name        = "terr_public_securitygroup"
+  description = "For public access security group, allow http https ssh inbound traffic"
+  vpc_id      = aws_vpc.terr_vpc.id
+#entering 
+#ssh
+ingress {
+    description      = "SSH "
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      =  ["0.0.0.0/0"]
+  }
+
+
+ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+
+    #exiting
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" 
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terr_public_securitygroup"
+  }
+}
+
+
+#private access security group
+resource "aws_security_group" "terr_private_sec_group" {
+  name        = "terr_private_securitygroup"
+  description = "Private access security group for db"
+  vpc_id      = aws_vpc.terr_vpc.id
+
+  ingress {
+    description      = "DB"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups = [aws_security_group.terr_public_sec_group.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terr_private_securitygroup"
+  }
+}
